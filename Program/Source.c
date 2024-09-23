cf8d4c5dcc2e9ef41f9bec9ab9f0d36649b66200
sukmin11 <sukmin1105@naver.com>
2024-09-23 21:44:43 +0900

    (2024.9.23) 피하기 게임

--- Program/Source.c
+++ Program/Source.c
@@ -1,8 +1,150 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+#define WIDTH 9
+#define HEIGHT 25
+
+typedef struct Character
+{
+	int x;
+	int y;
+	const char* shape;
+}Character;
+
+char map[HEIGHT][WIDTH] = {
+	{'1','1','1','1','1','1','1','1','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','0','0','0','0','0','0','0','1'},
+	{'1','1','1','1','1','1','1','1','1'},
+};
+
+void Render()
+{
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (map[i][j] == '0')
+				printf("  ");
+			else if (map[i][j] == '1')
+				printf("■");
+			else if (map[i][j] == '2')
+				printf("※");
+		}
+		printf("\n");
+	}
+}
+
+void Position(int x, int y)
+{
+	// X와 Y축을 설정하는 구조체입니다.
+	COORD position = { x, y };
+
+	// 콘솔 커서의 좌표를 설정하는 함수입니다.
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 
 int main()
 {
+	srand(time(NULL));
 
+	int hole = 0; 
+	int line = 1; 
+	char key = 0;
 
+	Character character = { 8,22, "△"};
+
+	hole = rand() % 7 + 1;
+
+	for (int i = 1; i < 8; i++)
+	{
+		map[line][i] = '2';
+	}
+
+	map[line][hole] = '0';
+
+	Render();
+
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case UP:
+				if (map[character.y - 1][character.x / 2] != '1')
+				{
+					character.y--;
+				}
+				break;
+			case LEFT:
+				if (map[character.y][character.x / 2 - 1] != '1')
+				{
+					character.x -= 2;
+				}
+				break;
+			case RIGHT:
+				if (map[character.y][character.x / 2 + 1] != '1')
+				{
+					character.x += 2;
+				}
+				break;
+			case DOWN:
+				if (map[character.y + 1][character.x / 2] != '1')
+				{
+					character.y++;
+				}
+				break;
+			default:printf("Exception\n");
+				break;
+			}
+
+			system("cls");
+
+			Render();
+
+		}
+		
+		line++;
+
+		Position(character.x, character.y);
+		printf("%s", character.shape);
+	}
 	return 0;
 }
